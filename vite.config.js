5d672a153aa2610cbe772d2b412df384d09c76e7
aleksi <aleksi.eskola100@gmail.com>
2024-02-18 13:37:26 +0200

    changed build dir to docs for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import prismjs from "vite-plugin-prismjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "./docs",
+  },
   plugins: [
     react(),
     prismjs({
